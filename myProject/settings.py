ca7769727c8e8f5c0f32cf1f1cad0ed852a69d53
Rafael Moreira Ramos de Rezende <100146657+UPraggy@users.noreply.github.com>
2022-12-16 15:37:30 -0300

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -181,12 +181,12 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 #EMAIL RESET
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "testemailur@gmail.com"
-EMAIL_HOST_PASSWORD = "utqkejpkjwcjllui"
-EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 
+EMAIL_USE_TLS = True'''
 
 #heroku
 
